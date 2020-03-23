b92b836f2a571c4d271fbaa8fb1005072e8a9817
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-23 02:05:30 -0300

    implement: create inputs for cliente data

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,28 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 
-		System.out.println( "Hello World!" );
+		Scanner sc = new Scanner( System.in );
+		sc.useLocale( Locale.US );
+		
+		SimpleDateFormat sdf = new SimpleDateFormat( "dd/MM/yyyy" );
+		
+		System.out.println( "Enter cliente data:" );
+		System.out.print( "Name: " );
+		String name = sc.next();
+		System.out.print( "Email: " );
+		String email = sc.next();
+		System.out.print( "Birth date (DD/MM/YYYY): " );
+		Date birthDay = sdf.parse( sc.next() );
+		
 		
 	}
 
