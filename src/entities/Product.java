3671ae95f0c8ed6215717030a730c851cff7872a
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-23 01:40:30 -0300

    setting: orderitem class

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -3,8 +3,11 @@ package entities;
 public class Product {
 
 	private String name;
-	private Double price;
+	private Double price;	
 	
+	public Product() {
+	}
+
 	public Product(String name, Double price) {
 		this.name = name;
 		this.price = price;
