736644f0b2f355e75b07d05c05c350df65ef86a3
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-03-23 02:13:43 -0300

    implement: create inputs for order data

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Client;
+
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
@@ -22,6 +24,24 @@ public class Program {
 		String email = sc.next();
 		System.out.print( "Birth date (DD/MM/YYYY): " );
 		Date birthDay = sdf.parse( sc.next() );
+		
+		System.out.println( "Enter order data:" );
+		System.out.print( "Status: " );
+		String status = sc.next();
+		System.out.print( "How many items to this order? " );
+		int numItems = sc.nextInt();
+		
+		for ( int i=0; i<numItems; i++ ) {
+			System.out.println( "Enter #"+ (i+1) +"item data:" );
+			System.out.print( "Product name: " );
+			String productName = sc.next();
+			System.out.print( "Product price: " );
+			double productPrice = sc.nextDouble();
+			System.out.print( "Quantity: " );
+			int quantity = sc.nextInt();
+			
+			
+		}
 		
 		
 	}
